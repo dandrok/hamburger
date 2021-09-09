d61322717777b683dcefb1574165ee5aed720d1d
dandrok <patryk.stusik@gmail.com>
2021-09-09 23:49:41 +0200

    hamburger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import styled from 'styled-components'
+
+const MenuLabel = styled.label`
+  background-color: #0099ff;
+  position: fixed;
+  top: 6rem;
+  right: 6rem;
+
+  height: 7rem;
+  width: 7rem;
+  cursor: pointer;
+  z-index: 1000;
+  box-shadow: 0 1rem 3rem rgba(182, 237, 200, 0.3);
+  text-align: center;
+`
+
+const Icon = styled.span`
+  position: relative;
+  background-color: ${(props) => (props.clicked ? 'transparent' : 'white')};
+  width: 3rem;
+  height: 2px;
+  display: inline-block;
+  margin-top: 3.5rem;
+  transition: all 0.3s;
+  &::before,
+  &::after {
+    content: '';
+    background-color: white;
+    width: 3rem;
+    height: 2px;
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    transition: all 0.3s;
+  }
+  &::before {
+    top: ${(props) => (props.clicked ? '0' : '-0.8rem')};
+    transform: ${(props) => (props.clicked ? 'rotate(135deg)' : 'rotate(0)')};
+  }
+  &::after {
+    top: ${(props) => (props.clicked ? '0' : '0.8rem')};
+    transform: ${(props) => (props.clicked ? 'rotate(-135deg)' : 'rotate(0)')};
+  }
+  ${MenuLabel}:hover &::before {
+    top: ${(props) => (props.clicked ? '0' : '-1rem')};
+  }
+  ${MenuLabel}:hover &::after {
+    top: ${(props) => (props.clicked ? '0' : '1rem')};
+  }
+`
+
+export default function App() {
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <MenuLabel htmlFor='navi-toggle' onClick={handleClick}>
+        <Icon clicked={click}>&nbsp;</Icon>
+      </MenuLabel>
+    </>
+  )
 }
-
-export default App;
